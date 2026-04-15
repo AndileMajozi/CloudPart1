8ae175122cc8be135e19b95efbcc8837926f89a6
AndileMajozi <ST10338346@vcconnect.edu.za>
2026-04-15 21:30:47 +0200

    Update VenuesController.cs

--- EventEase/Controllers/VenuesController.cs
+++ EventEase/Controllers/VenuesController.cs
@@ -19,7 +19,7 @@ namespace EventEase.Controllers
             _context = context;
         }
 
-        // GET: Venues
+        
         public async Task<IActionResult> Index()
         {
             return View(await _context.Venues.ToListAsync());
